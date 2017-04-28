6210ed5403f6f97c7f8e956564b353763161d810
武纪昀 <386896769@qq.com>
2017-04-28 13:37:58 +0800

    修改

--- githubtest/billcheck-web/src/main/java/com/bosssoft/einvoice/billcheck/basic/entity/CodeConstant.java
+++
@@ -1,6 +0,0 @@
-package com.bosssoft.einvoice.billcheck.basic.entity;
-
-public class CodeConstant {
-
-  public static final ResultCode CSRF_ERROR = new ResultCode("101", "CSRF ERROR:无效的token，或者token过期");
-}
